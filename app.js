ee142905425668db2fb857dff60b68ed7e299107
TomozQ <inf.4clover@gmail.com>
2021-11-13 20:45:15 +0900

    never

--- app.js
+++ app.js
@@ -1,19 +1,13 @@
-function add(n1, n2) {
-    return n1 + n2;
+var userInput;
+var userName;
+userInput = 5;
+userInput = 'Max';
+if (typeof userInput === 'string') {
+    userName = userInput;
 }
-function printResult(num) {
-    console.log('Result' + num);
+function generateError(message, code) {
+    throw { message: message, errorCode: code };
+    // while (true){}
 }
-function addAndHandle(n1, n2, cb) {
-    var result = n1 + n2;
-    cb(result);
-}
-addAndHandle(10, 20, function (result) {
-    console.log(result);
-});
-var combineValues; //関数であることと、引数と戻り値の型を定義　function型
-combineValues = add;
-// combineValues = printResult
-// combineValues = 5
-console.log(combineValues(8, 8));
-printResult(add(5, 12));
+var result = generateError('エラーが発生しました。', 500);
+console.log(result);
--- app.ts
+++ app.ts
@@ -7,3 +7,11 @@ userInput = 'Max'
 if(typeof userInput === 'string'){
     userName = userInput
 }
+
+function generateError(message: string, code: number): never{  //絶対にありえない　絶対に戻り値がない 確実にコードがクラッシュする
+    throw { message: message, errorCode: code }
+    // while (true){}
+}
+
+const result = generateError('エラーが発生しました。', 500)
+console.log(result)
