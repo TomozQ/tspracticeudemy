0e208688bbf80621c0ae8783c8589a32e0936597
TomozQ <inf.4clover@gmail.com>
2021-11-13 17:14:14 +0900

    Role

--- app.ts
+++ app.ts
@@ -2,16 +2,33 @@
 //     name: string;
 //     age: number;
 // } = {
-const person: {
-    name: string
-    age: number
-    hobbies: string[]
-    role: [number, string]
-} = {
+// const person: {
+//     name: string
+//     age: number
+//     hobbies: string[]
+//     role: [number, string]
+// } = {
+//     name: 'tomoki',
+//     age: 30,
+//     hobbies: ['sports', 'cokkies'],
+//     role: [2, 'author'],
+// }
+
+// const ADMIN = 0
+// const READ_ONLY = 1
+// const AUTHER = 2
+
+enum Role {    //enumの定義
+    ADMIN = 'ADMIN' ,
+    READ_ONLY = 100, 
+    AUTHOR = 200,
+}
+
+const person = {
     name: 'tomoki',
     age: 30,
     hobbies: ['sports', 'cokkies'],
-    role: [2, 'author'],
+    role: Role.ADMIN,
 }
 
 // person.role.push('admin') pushは許可されてしまう。
@@ -27,4 +44,8 @@ console.log(person.name)
 for (const hobby of person.hobbies) {
     console.log(hobby.toUpperCase())
     // console.log(hobby.map())
-} 
+}
+
+if (person.role === Role.ADMIN){
+    console.log('管理者ユーザー')
+}
