b537b17e5ea71a746d6b299b610afed26bfcb3da
TomozQ <inf.4clover@gmail.com>
2021-11-13 20:33:28 +0900

    callback return

--- app.js
+++ app.js
@@ -4,6 +4,13 @@ function add(n1, n2) {
 function printResult(num) {
     console.log('Result' + num);
 }
+function addAndHandle(n1, n2, cb) {
+    var result = n1 + n2;
+    cb(result);
+}
+addAndHandle(10, 20, function (result) {
+    console.log(result);
+});
 var combineValues; //関数であることと、引数と戻り値の型を定義　function型
 combineValues = add;
 // combineValues = printResult
--- app.ts
+++ app.ts
@@ -6,6 +6,16 @@ function printResult(num: number): void { //void...return命令を持たない�
     console.log('Result' + num)
 }
 
+function addAndHandle(n1: number, n2: number, cb: (num: number) => (void)){
+    const result = n1 + n2
+    cb(result)
+}
+
+addAndHandle(10, 20, (result) => {
+    console.log(result)
+    return result
+})
+
 let combineValues: (a: number, b: number) => number //関数であることと、引数と戻り値の型を定義　function型
 
 combineValues = add
