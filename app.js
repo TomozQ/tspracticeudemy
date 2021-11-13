3a10a6baaeb9edac78ed2745d73af47e2ed38b46
TomozQ <inf.4clover@gmail.com>
2021-11-13 17:59:56 +0900

    function型

--- app.js
+++ app.js
@@ -4,4 +4,9 @@ function add(n1, n2) {
 function printResult(num) {
     console.log('Result' + num);
 }
+var combineValues; //関数であることと、引数と戻り値の型を定義　function型
+combineValues = add;
+// combineValues = printResult
+// combineValues = 5
+console.log(combineValues(8, 8));
 printResult(add(5, 12));
--- app.ts
+++ app.ts
@@ -6,4 +6,12 @@ function printResult(num: number): void { //void...return命令を持たない�
     console.log('Result' + num)
 }
 
+let combineValues: (a: number, b: number) => number //関数であることと、引数と戻り値の型を定義　function型
+
+combineValues = add
+// combineValues = printResult
+// combineValues = 5
+
+console.log(combineValues(8, 8))
+
 printResult(add(5,12))
