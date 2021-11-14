a891d570819d430a31bbb6d275df3f1702abb6a9
TomozQ <inf.4clover@gmail.com>
2021-11-14 23:40:23 +0900

    class constructor, public, private

--- dist/app.js
+++ dist/app.js
@@ -1,28 +1,24 @@
 "use strict";
-const button = document.querySelector('button');
-if (button) {
-    button.addEventListener('click', e => {
-        console.log(e);
-    });
-}
-const hobbies = ['sports', 'cokking'];
-const activeHobbies = ['hiking', ...hobbies];
-console.log(activeHobbies);
-const person = {
-    firstname: 'max',
-    age: 30
-};
-const copiedPerson = Object.assign({}, person);
-console.log(copiedPerson);
-const add = (...numbers) => {
-    return numbers.reduce((curResult, curValue) => {
-        return curResult + curValue;
-    }, 0);
-};
-const addedNumbers = add(5, 10, 2, 3.7);
-console.log(addedNumbers);
-const [hobby1, hobby2, ...remaingHobbies] = hobbies;
-console.log(hobbies, hobby1, hobby2);
-const { firstname: userName, age } = person;
-console.log(userName, age, person);
+var Department = (function () {
+    function Department(n) {
+        this.employees = [];
+        this.name = n;
+    }
+    Department.prototype.describe = function () {
+        console.log("Department: " + this.name);
+    };
+    Department.prototype.addEmployee = function (employee) {
+        this.employees.push(employee);
+    };
+    Department.prototype.printEmployeeInformation = function () {
+        console.log(this.employees.length);
+        console.log(this.employees);
+    };
+    return Department;
+}());
+var accounting = new Department('Accounting');
+accounting.addEmployee('Max');
+accounting.addEmployee('Manu');
+accounting.describe();
+accounting.printEmployeeInformation();
 //# sourceMappingURL=app.js.map
--- src/app.ts
+++ src/app.ts
@@ -1,84 +1,38 @@
-// const useerName = 'Max'
-// userName = 'Maximilian'
-// let age = 30
+class Department {
+    name: string
+    private employees: string[] = [] // private クラスの内部からのみアクセスできる
 
-// age = 29
+    constructor(n: string) {
+        this.name = n
+    }
 
-// function add(a: number, b: number){
-//     let result
-//     result = a+b
-//     return result
-// }
+    describe(this: Department){  //ダミーパラメータを使用することで堅牢にする
+        console.log("Department: " + this.name)
+    }
 
-// if (age >= 20){
-//     let isAdult = true
-// }
+    addEmployee(employee: string){
+        this.employees.push(employee)
+    }
 
-// console.log(isAdult)
-
-//const, letはブロック内でしか使用できないようになっている（varはアクセスできる）
-
-// const add = (a: number, b: number = 1) =>  a+b  //デフォルト値は右側のパラメータにしか設定できない
-
-// const printOutput: (output: string | number) => void = output => {
-//     console.log(output)
-// }
-
-
-// printOutput(add(2))
-
-const button = document.querySelector('button')
-
-if (button){
-    button.addEventListener('click', e => {
-        console.log(e)
-    })
+    printEmployeeInformation(){
+        console.log(this.employees.length)
+        console.log(this.employees)
+    }
 }
 
-const hobbies = ['sports', 'cokking']
-// console.log(hobbies[0])
-const activeHobbies = ['hiking', ...hobbies] //スプレッド演算子
-// activeHobbies.push(...hobbies)
+const accounting = new Department('Accounting')
 
-console.log(activeHobbies)
+// console.log(accounting)
 
-const person = {
-    firstname: 'max',
-    age: 30
-}
+accounting.addEmployee('Max')
+accounting.addEmployee('Manu')
 
-// const copiedPerson = person  //参照のコピー
+// accounting.employees[2] = 'Anna'　//employeesがprivateなのでアクセスできない 最近のprivate, publicの別があるJSであれば大丈夫だがそれ以外は実際にはコンパイルしてアクセスできてしまう
 
-const copiedPerson = { //オブジェクトのコピー
-    ...person,
-}
+accounting.describe()
+accounting.printEmployeeInformation()
 
-console.log(copiedPerson)
 
-//rest parameter
-const add = (...numbers: number[]) => { //rest parameter 任意の数の引数を取得することができる。
-    return numbers.reduce((curResult, curValue) => {  //reduce -> 配列に扱える関数
-        return curResult + curValue
-    }, 0) //curResult->現在の計算結果、curValue->現在の要素（numbersの要素がひとつずつ入ってくる）
-}
-// const add = (...numbers: [number, number, number]) => { //tupleとの組み合わせも可能
-//     return numbers.reduce((curResult, curValue) => { 
-//         return curResult + curValue
-//     }, 0) 
-// }
+// const accountingCopy = { name: 'Dumy', describe: accounting.describe }  //Departmentクラスと同じプロパティであればTypeScriptはエラーを吐かない
 
-const addedNumbers = add(5,10,2,3.7)
-console.log(addedNumbers)
-
-// const addedNumbers = add(5,10,2)
-// console.log(addedNumbers)
-
-// 分割代入
-// const hobby1 = hobbies[0]
-// const hobby2 = hobbies[1]
-
-const [hobby1, hobby2, ...remaingHobbies] = hobbies //分割代入 + スプレッド演算子　1つ目と2つ目以外の残りの要素はremaingHobbiesという配列に代入される
-console.log(hobbies, hobby1, hobby2)  //配列の値をコピー　コピー元の要素は削除されない
-
-const { firstname: userName, age } = person  //オブジェクトの分割代入　オブジェクトに存在するプロパティ名でないとならない 「: 定数」とすることで定数名を上書きできる。
-console.log(userName, age, person)
+// accountingCopy.describe()
