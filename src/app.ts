33457cecc86c23840c0188931cba932ca1e25784
TomozQ <inf.4clover@gmail.com>
2021-11-16 23:29:23 +0900

    class implements interface

--- src/app.ts
+++ src/app.ts
@@ -1,18 +1,26 @@
 //interface -> objectがどんな形のものであるか定義するためのもの
-interface Person {
+// type Person = {  //typeとinterfaceは互換性がある。 interfaceはオブジェクトの構造を定義するためだけに使用できる
+interface Greetable  {
     name: string
-    age: number
     greet(phrase: string): void
 }
 
-let user1: Person   //Personで定義されている内容をプロパティにもたなくてはならない
-
-user1 = {   //interfaceと同じ型のものが定義されているかをチェックされる
-    name: 'Max',
-    age: 30,
-    greet(phrase: string) {
+class Person implements Greetable {  //interfaceを実装する　複数定義できる
+    name: string
+    age = 30
+    constructor(n: string){
+        this.name = n
+    }
+    greet(phrase: string){
         console.log(phrase + '' + this.name)
     }
-}
+}   
+
+let user1: Greetable   //Personで定義されている内容をプロパティにもたなくてはならない
+
+user1 = new Person('Max')   //上でGreetableと型定義しているが、PersonクラスはGreetableインターフェースを実装しているので問題なくインスタンス化できる。
+//interfaceに基づいたオブジェクトを作成している。
 
 user1.greet('Hello i am ')
+
+console.log(user1)
