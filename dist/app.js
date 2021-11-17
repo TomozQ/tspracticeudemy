000ed47ca575403b68a7fb41b4b71828f8a657ca
TomozQ <inf.4clover@gmail.com>
2021-11-17 23:10:26 +0900

    type guard

--- dist/app.js
+++ dist/app.js
@@ -4,4 +4,49 @@ var e1 = {
     privilages: ['create-server'],
     startDate: new Date,
 };
+function add(a, b) {
+    if (typeof a === 'string' || typeof b === 'string') {
+        return a.toString() + b.toString();
+    }
+    return a + b;
+}
+function printEmployeeInformation(emp) {
+    console.log(emp.name);
+    if ('privilages' in emp) {
+        console.log("privilages" + emp.privilages);
+    }
+    if ('startDate' in emp) {
+        console.log("startDate" + emp.startDate);
+    }
+}
+printEmployeeInformation({ name: 'manu', startDate: new Date() });
+var Car = (function () {
+    function Car() {
+    }
+    Car.prototype.drive = function () {
+        console.log('運転中');
+    };
+    return Car;
+}());
+var Track = (function () {
+    function Track() {
+    }
+    Track.prototype.drive = function () {
+        console.log('トラックを運転中');
+    };
+    Track.prototype.loadCargo = function (amount) {
+        console.log('荷物を載せています・・・' + amount);
+    };
+    return Track;
+}());
+var v1 = new Car();
+var v2 = new Track();
+function useVehcle(vehicle) {
+    vehicle.drive();
+    if (vehicle instanceof Track) {
+        vehicle.loadCargo(1000);
+    }
+}
+useVehcle(v1);
+useVehcle(v2);
 //# sourceMappingURL=app.js.map
--- src/app.ts
+++ src/app.ts
@@ -22,3 +22,55 @@ type Cominable = string | number
 type Numeric = number | boolean
 
 type Universal = Cominable & Numeric
+
+function add ( a:Cominable, b: Cominable ){
+    if(typeof a === 'string' || typeof b === 'string'){ //typeガード
+        return a.toString() + b.toString()
+    }
+    return a + b
+}
+
+type UnknownEmployee = Employee | Admin
+
+function printEmployeeInformation(emp: UnknownEmployee){
+    console.log(emp.name)
+    if('privilages' in emp){
+        console.log("privilages" + emp.privilages)
+    }
+    if('startDate' in emp){
+        console.log("startDate" + emp.startDate)
+    }
+}
+
+printEmployeeInformation({name: 'manu', startDate: new Date()})
+
+class Car {
+    drive(){
+        console.log('運転中')
+    }
+}
+
+class Track{
+    drive(){
+        console.log('トラックを運転中')
+    }
+
+    loadCargo(amount: number){
+        console.log('荷物を載せています・・・' + amount)
+    }
+}
+
+type Vehcle = Car | Track
+
+const v1 = new Car()
+const v2 = new Track()
+
+function useVehcle(vehicle: Vehcle){
+    vehicle.drive()
+    if(vehicle instanceof Track){  //instanceof での型ガード
+        vehicle.loadCargo(1000)
+    }
+}
+
+useVehcle(v1)
+useVehcle(v2)
