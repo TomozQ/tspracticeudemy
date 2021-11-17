4ae372a4092898670ee1e98fb8a36241bfc9e2d5
TomozQ <inf.4clover@gmail.com>
2021-11-17 22:48:20 +0900

    交差型の型定義

--- dist/app.js
+++ dist/app.js
@@ -1,27 +1,7 @@
 "use strict";
-var add;
-add = function (n1, n2) {
-    return n1 + n2;
+var e1 = {
+    name: 'Max',
+    privilages: ['create-server'],
+    startDate: new Date,
 };
-var Person = (function () {
-    function Person(n) {
-        this.age = 30;
-        if (n) {
-            this.name = n;
-        }
-    }
-    Person.prototype.greet = function (phrase) {
-        if (this.name) {
-            console.log(phrase + '' + this.name);
-        }
-        else {
-            console.log('Hi');
-        }
-    };
-    return Person;
-}());
-var user1;
-user1 = new Person();
-user1.greet('Hello i am ');
-console.log(user1);
 //# sourceMappingURL=app.js.map
---
+++ src/interfaces.ts
@@ -0,0 +1,48 @@
+// type AddFn = (a: number, b: number) => number ->こちらの関数型の定義が一般的
+interface AddFn {
+    (a: number, b:number): number   //関数型を定義
+}
+
+let add: AddFn
+
+add = (n1: number, n2: number) => {
+    return n1 + n2
+}
+//interface -> objectがどんな形のものであるか定義するためのもの
+interface Named {
+    readonly name?: string
+    outputName?: string  //このプロパティを持っているかどうかはどちらでも良い
+}
+// type Person = {  //typeとinterfaceは互換性がある。 interfaceはオブジェクトの構造を定義するためだけに使用できる
+interface Greetable extends Named{  //Namedを継承 複数のinterfaceを継承することができる。
+    greet(phrase: string): void
+}
+
+class Person implements Greetable {  //interfaceを実装する　複数定義できる
+    name?: string //オプショナル
+    age = 30
+
+    constructor(n?: string){
+        if(n){
+            this.name = n
+        }
+    }
+    greet(phrase: string){
+        if(this.name){
+
+            console.log(phrase + '' + this.name)
+        }else{
+            console.log('Hi')
+        }
+    }
+}   
+
+let user1: Greetable   //Personで定義されている内容をプロパティにもたなくてはならない
+
+user1 = new Person()   //上でGreetableと型定義しているが、PersonクラスはGreetableインターフェースを実装しているので問題なくインスタンス化できる。
+//interfaceに基づいたオブジェクトを作成している。
+// user1.name = 'Mane' interfaceのreadonlyを利用している。
+
+user1.greet('Hello i am ')
+
+console.log(user1)
