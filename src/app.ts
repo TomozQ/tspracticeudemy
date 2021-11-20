59d6ee8ec190d9466e9cbfa25ed76c196585c268
TomozQ <inf.4clover@gmail.com>
2021-11-20 23:04:29 +0900

    様々なデコレーター

--- src/app.ts
+++ src/app.ts
@@ -42,3 +42,57 @@ class Person {
 const pers = new Person()
 
 console.log(pers)
+
+
+//---
+
+function Log(target: any, propertyname: string | Symbol){
+    console.log('Property Decorator')
+    console.log(target, propertyname)
+}
+
+function Log2(target: any, name: string, descriptor: PropertyDescriptor){
+    console.log("Accessor decorator")
+    console.log(target)
+    console.log(name)
+    console.log(descriptor)
+}
+
+function Log3(target: any, name: string | Symbol, descriptor: PropertyDescriptor){
+    console.log("Method decorator")
+    console.log(target)
+    console.log(name)
+    console.log(descriptor)
+}
+
+function Log4(target: any, name: string | Symbol, position: number){
+    console.log("Parameter decorator")
+    console.log(target)
+    console.log(name)
+    console.log(position)
+}
+
+class Product{
+    @Log                        //プロパティデコレーター    クラスのプロパティがクラスの一部としてjavascriptで定義されたとき -> コンストラクターが実行されたとき
+    title: string
+    private _price: number
+
+    @Log2
+    set price(val: number){     //アクセサーデコレーター
+        if (val > 0){
+            this._price = val
+        }else{
+            throw new Error('不正な価格です - 0以下は設定できません。')
+        }
+    }
+
+    constructor(t: string, p: number){
+        this.title = t
+        this._price = p
+    }
+
+    @Log3                                   //メソッドパラメーター
+    getPriceWithTax(@Log4 tax: number){     // パラメータデコレーター
+        return this._price * (1 + tax)
+    }
+}
