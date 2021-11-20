2eda0a4e282de41a1b4042377253e888e3f3cf65
TomozQ <inf.4clover@gmail.com>
2021-11-20 19:42:27 +0900

    decorator factory

--- src/app.ts
+++ src/app.ts
@@ -12,9 +12,22 @@ function Logger(logString: string) {        //デコレーターファクトリ�
     }
 }
 
+function WithTemplate(template: string, hookId: string){
+    // return function(_: Function){  //引数を'_'にすることによって必要だが使用しないという意味合いになる　引数としては書いておく必要がある。
+
+    return function(constructor: any){                          //Personクラスのconstructorを引数に取る
+        const hookEl = document.getElementById(hookId)          //要素を取得
+        const p = new constructor()                             //インスタンスを作成
+        if(hookEl){
+            hookEl.innerHTML = template
+            hookEl.querySelector('h1')!.textContent = p.name
+        }
+    }
+}
 
 // @Logger                                             //デコレーター -> クラス定義のタイミングで呼びだされる。インスタンスが作成されるタイミングではない。
-@Logger('ログ出力中 - Person')                          
+// @Logger('ログ出力中 - Person')     
+@WithTemplate('<h1>Personオブジェクト</h1>', 'app')                     
 class Person {
     name = 'Max'
 
