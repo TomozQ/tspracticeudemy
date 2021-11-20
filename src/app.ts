212b5525143c9f799e652be400dd5a58b2226868
TomozQ <inf.4clover@gmail.com>
2021-11-20 19:28:25 +0900

    decorator factory

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,20 @@
 // tsconfig.json -> "experimentalDecorators": trueのコメントアウトを外す
 
-function Logger(constructor: Function) {
-    console.log('Log出力中...')
-    console.log(constructor)
+// function Logger(constructor: Function) {        //デコレーター関数
+//     console.log('Log出力中...')
+//     console.log(constructor)
+// }
+
+function Logger(logString: string) {        //デコレーターファクトリー
+    return function(constructor: Function){
+        console.log(logString)
+        console.log(constructor)
+    }
 }
 
 
-@Logger                                             //デコレーター -> クラス定義のタイミングで呼びだされる。インスタンスが作成されるタイミングではない。
+// @Logger                                             //デコレーター -> クラス定義のタイミングで呼びだされる。インスタンスが作成されるタイミングではない。
+@Logger('ログ出力中 - Person')                          
 class Person {
     name = 'Max'
 
